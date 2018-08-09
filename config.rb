7adaa8e78be09e916945af9386cb600106d64a0c
Param Pranam <42201127+parampranam@users.noreply.github.com>
2018-08-09 22:30:42 +0530

    committed.
    Usage: $ compass init

--- config.rb
+++ config.rb
@@ -1,1 +1,28 @@
 #!/usr/bin/env ruby
+
+
+# ----------------
+# heyprabhat®
+# ----------------
+# Hey Prabhat — A Personal Writings.
+# ___________________________________________________________________________
+#
+# Architecture and Code Handcrafted by Prabhat Kumar.
+# Architectuur en Code handgemaakt door Prabhat Kumar.
+# @author    : Prabhat Kumar [http://prabhatkumar.org/].
+# @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+# @copyright : Sequømics Research [http://research.sequomics.com/].
+# @copyright : Sequømics Corporation [http://sequomics.com/].
+# ___________________________________________________________________________
+#
+# @date      : 09-Aug-2018
+# @license   : Apache, version 2.0
+# @require   : Ruby
+# @require   : Sass
+# @require   : Compass
+# @build     : SEED™ — Umeå
+#              └────── A Sequømics Product — http://sequomics.com/.
+# ___________________________________________________________________________
+
+
+# Usage: $ compass init
