8f0d8a09aacf411ea327e65721c04867354b5974
Param Pranam <42201127+parampranam@users.noreply.github.com>
2018-08-09 14:26:13 +0530

    committed.
    build.sh, shell script to --/build/-- whole "heyprabhat®" in one go.
    Usage: $ sh build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@
 #     Copyright © 2006 - 2018, Prabhat Kumar. All rights reserved.
 # Copyright © 2014 - 2018, Sequømics Corporation, All rights reserved.
 # ====================================================================
+
+# Shell Script:—
