9060fa874a78733284d9f295b5812b0990db6144
Param Pranam <42201127+parampranam@users.noreply.github.com>
2018-08-09 14:28:35 +0530

    committed.
    clean.sh, shell script to --/clean/-- whole "heyprabhat®" in one go.
    Usage: $ sh clean.sh

--- clean.sh
+++ clean.sh
@@ -4,3 +4,6 @@
 #     Copyright © 2006 - 2018, Prabhat Kumar. All rights reserved.
 # Copyright © 2014 - 2018, Sequømics Corporation, All rights reserved.
 # ====================================================================
+
+# Shell Script:—
+# 🐚
