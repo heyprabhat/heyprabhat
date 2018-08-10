9ff27b5ea88c1eed69af6c9ead0ffbfe02e060ea
Param Pranam <42201127+parampranam@users.noreply.github.com>
2018-08-10 10:44:13 +0530

    committed.
    setup.py, the setup script is the centre of all activity in building, distributing, and installing modules.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__company__ 	= 'Sequømics Corporation'
+__homepage__ 	= 'http://sequomics.com/'
+__account__ 	= 'heyprabhat'
+__githubURL__ 	= 'https://github.com/heyprabhat'
+__authors__ 	= [
+    '"Prabhat Kumar" <pranam.prabhat@gmail.com>',
+    '"Sequømics Corporation" <admin@sequomics.com>'
+    ]
+__license__     = 'Apache License'
+
+# ------------------------------------------------------------------------
+# Copyright © 2014 - 2018, Sequømics Corporation. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0, (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#       	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========================================================================
