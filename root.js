c97bf19de436cf76aca098a12b593c303aca751a
Param Pranam <42201127+parampranam@users.noreply.github.com>
2018-08-10 13:15:30 +0530

    committed.
    ¶ https://nodejs.org/en/.

---
+++ root.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+
+/*!
+ * heyprabhat
+ * From the Desk of Prabhat Kumar — CEO, Founder & Scientist.
+ * ___________________________________________________________________________
+ *
+ * Node, https://nodejs.org/en/.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 10-Aug-2018
+ * @license   : MIT
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-cli
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "heyprabhat®".
+ * ___________________________________________________________________________
+ */
+
+// "disallowMultipleSpaces": {"allowEOLComments": true}
+// "disallowSemicolons": false
+// "requireSemicolons": true
+// "requireSpaceAfterLineComment": { "allExcept": ["#", "="] }
+
+// global __dirname: true
+// global require: true
+
+//# Usage: $ node -v
+//# Usage: $ npm -v
+//# Usage: $ grunt -version
+
+// Invoking strict mode.
+// @purpose: Strict mode applies to entire scripts or to individual functions.
+"use strict";
+
+// To load required Node module.
+///-----------------------------
+var fs    = require('fs');
+var path  = require('path');
+
+// To load required NPM modules.
+// -----------------------------
+var chalk = require('chalk');
+var glob  = require('glob');
+
+// Default color defined.
+// ----------------------
+var noop  = chalk.red;
+var yeep  = chalk.green;
+var okay  = chalk.blue;
+var boop  = chalk.yellow;
+
+var name  = 'Prabhat Kumar';
+console.log(chalk.green('Hello! %s'), name);
+//=> 'Hello!, Prabhat Kumar.'
